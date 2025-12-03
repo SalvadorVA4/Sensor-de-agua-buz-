523fde4f8067a7626074c619a78011c35d874a7f
SalvadorVA4 <villalpandoalcubillas@gmail.com>
2025-12-02 20:36:44 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace LLuvia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.botonComenza = new System.Windows.Forms.Button();
+            this.botonSalir = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // botonComenza
+            // 
+            this.botonComenza.BackColor = System.Drawing.Color.Red;
+            this.botonComenza.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonComenza.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.botonComenza.Location = new System.Drawing.Point(78, 242);
+            this.botonComenza.Name = "botonComenza";
+            this.botonComenza.Size = new System.Drawing.Size(155, 58);
+            this.botonComenza.TabIndex = 0;
+            this.botonComenza.Text = "Comenzar";
+            this.botonComenza.UseVisualStyleBackColor = false;
+            this.botonComenza.Click += new System.EventHandler(this.botonComenza_Click);
+            // 
+            // botonSalir
+            // 
+            this.botonSalir.BackColor = System.Drawing.Color.Red;
+            this.botonSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonSalir.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.botonSalir.Location = new System.Drawing.Point(78, 336);
+            this.botonSalir.Name = "botonSalir";
+            this.botonSalir.Size = new System.Drawing.Size(155, 58);
+            this.botonSalir.TabIndex = 1;
+            this.botonSalir.Text = "Salir";
+            this.botonSalir.UseVisualStyleBackColor = false;
+            this.botonSalir.Click += new System.EventHandler(this.botonSalir_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(68, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(604, 46);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Sensor de lluvia FC-37 o YL-83";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(70, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(271, 36);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Estado del sensor";
+            // 
+            // bindingSource1
+            // 
+            this.bindingSource1.CurrentChanged += new System.EventHandler(this.bindingSource1_CurrentChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(486, 136);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(258, 106);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.botonSalir);
+            this.Controls.Add(this.botonComenza);
+            this.Name = "Form1";
+            this.Text = "Sensor de agua o lluvia";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button botonComenza;
+        private System.Windows.Forms.Button botonSalir;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LLuvia
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPort;
+        private bool isBlinking;
+        private SoundPlayer soundPlayer;
+    
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort = new SerialPort("COM3", 9600);
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+            botonComenza.Click += botonComenza_Click;
+            timer1.Interval = 500;// Intervalo de parpadeo en milisegundos
+            timer1.Tick += timer1_Tick;
+            soundPlayer = new SoundPlayer("C:\\Users\\miria\\OneDrive\\Escritorio\\2024 Semestre 2\\MECANICA\\gifs\\Emergency Siren Police Wail 02.wav");
+        }
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+            base.OnFormClosing(e);
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            {
+                throw new NotImplementedException();
+            }
+
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void botonComenza_Click(object sender, EventArgs e)
+        {
+            if (!serialPort.IsOpen)
+            {
+                serialPort.Open();
+            }
+            void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e )
+            {
+                string data = serialPort.ReadLine();
+                this.Invoke(new Action(() => {
+                    label1.Text = "Sensor Status: " + (data.Trim() == "1" ? "Water Detected" : "No Water Detected");
+                    if (data.Trim() == "1")
+                    {
+                        if (!isBlinking)
+                        {
+                            timer1.Start();
+                            soundPlayer.PlayLooping();
+                            pictureBox1.BackColor = System.Drawing.Color.Red;
+                            isBlinking = true;
+                        }
+                    }
+                    else
+                    {
+                        if (isBlinking)
+                        {
+                            soundPlayer.Stop();
+                            pictureBox1.BackColor = System.Drawing.Color.Gray;
+                            isBlinking = false;
+                        }
+                    }
+                }));
+            }
+        }
+
+        private void botonSalir_Click(object sender, EventArgs e)
+        {
+            {
+                serialPort.Close();
+                Application.Exit();
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.BackColor = pictureBox1.BackColor == System.Drawing.Color.Gray ? System.Drawing.Color.Yellow : System.Drawing.Color.Gray;
+        }
+    }
+}
